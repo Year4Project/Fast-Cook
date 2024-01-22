83df3837d768f4598da9cdcb31d995f30631f628
BRAKTHIN <bbrakthin@Bbrakthins-MBP.netis.cc>
2024-01-22 23:49:14 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+import ReactRefresh from '@vitejs/plugin-react';
+
+export default {
+  plugins: [ReactRefresh()],
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+    outDir: 'dist',
+    assetsDir: '.',
+    minify: 'terser',
+    sourcemap: true,
+    rollupOptions: {
+      // Add the following line to specify the jsx loader
+      // for files with the .js extension
+      plugins: [{ name: 'vite-plugin-jsx' }],
+    },
+  },
+};
