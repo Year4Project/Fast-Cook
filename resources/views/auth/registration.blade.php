7ca0cb791f40c471edf7aaf794e0d342ef7642f6
BRAKTHIN <bbrakthin@Bbrakthins-MacBook-Pro.local>
2024-05-07 14:16:30 +0700

    add logo

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -4,6 +4,7 @@
 <head>
 
     <meta charset="utf-8">
+    <link rel="shortcut icon" type="x-icon" href="{{ asset('admin/img/logo.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
