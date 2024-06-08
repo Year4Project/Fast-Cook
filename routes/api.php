4d10f5a4e669848d27a342d6c0e6cd823ec3e986
BRAKTHIN <bbrakthin@Bbrakthins-MacBook-Pro.local>
2024-06-08 10:58:23 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ use Illuminate\Support\Facades\Auth;
 */
 
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 Route::middleware(['apikey', 'auth:api', 'jwt.auth'])->group(function () {
